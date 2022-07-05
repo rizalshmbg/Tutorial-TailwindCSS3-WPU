33d9368359220e6b346be905990ad999996b6cd3
rizalshmbg <sihombingrizal18@gmail.com>
2022-07-05 20:06:12 +0700

    finish the 7th video

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,23 @@ module.exports = {
 			spacing: {
 				13: '3.25rem',
 			},
-      fontFamily: {
-        inter: ['Inter']
-      },
-      colors: {
-        rizal: '#BADA55',
-        kopi: 'C0FFEE'
-      }
+			fontFamily: {
+				inter: ['Inter'],
+			},
+			colors: {
+				rizal: '#BADA55',
+				kopi: 'C0FFEE',
+			},
+			animation: {
+				'spin-slow': 'spin 3s linear infinite',
+				'wiggle': 'wiggle 1s ease-in-out infinite'
+			},
+			keyframes: {
+				wiggle: {
+					'0%, 100%' : { transform: 'rotate(-3deg)' },
+					'50%' : { transform: 'rotate(3deg)' },
+				}
+			},
 		},
 	},
 	plugins: [],
